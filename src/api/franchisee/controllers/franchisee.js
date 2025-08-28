2d66a679bd64a970afc8e37574c8e6d4bdedf4d6
Richard Pham <vcpham@scu.edu>
2025-08-28 11:09:14 -0700

    new content types

---
+++ src/api/franchisee/controllers/franchisee.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * franchisee controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::franchisee.franchisee');
---
+++ src/api/franchisee/services/franchisee.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * franchisee service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::franchisee.franchisee');
